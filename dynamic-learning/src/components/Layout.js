dc3acac2ca82167841455be62e4fe341feab40fb
Parixit Singh <pst.singh5@gmail.com>
2024-06-05 08:25:47 +0530

    added header logic

--- dynamic-learning/src/components/Layout.js
+++ dynamic-learning/src/components/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavigationHeader from './Controls/NavigationHeader';
+import Footer from './Controls/Footer';
 
 function Layout(props) { 
   return (
     <>
         <NavigationHeader data={props.data}/>
-        <Outlet />
+        <div className='content'>
+          <Outlet />
+        </div>
+        <Footer />
     </>
   )
 }
